45a15b88aae4a84b18b4df4017376380cfb39d69
candye_sweet <olyamelska@Olhas-MBP.home>
2019-01-28 23:14:18 -0500

    Creating a dropdown menu

--- assets/JavaScript/java.js
+++ assets/JavaScript/java.js
@@ -2,6 +2,21 @@ let navbar = document.getElementById("navbar");
 let button = document.getElementById("submitButton");
 let sticky = navbar.offsetTop;
 
+let menuIcon = document.getElementById("menu-icon");
+let about = document.getElementById("about");
+let portfolio = document.getElementById("portfolio");
+let contact = document.getElementById("contact");
+
+menuIcon.addEventListener("click", value => {
+  about.innerHTML = `<a href="../../index.html">About</a>`;
+  about.classList.add("dropdown-menu");
+  portfolio.innerHTML = `<a href="../../portfolio.html">Portfolio</a>`;
+  portfolio.classList.add("dropdown-menu");
+  contact.innerHTML = `<a href="../../contact.html">Contact</a>`;
+  contact.classList.add("dropdown-menu");
+  console.log("you clicked");
+});
+
 window.onscroll = function() {
   fixedNavbar();
 };
@@ -13,5 +28,3 @@ function fixedNavbar() {
     navbar.classList.remove("sticky");
   }
 }
-
-console.log("message");
