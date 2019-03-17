f2c7369b96e466d057caac70b15eb26464e6171e
candye_sweet <olya.melska@gmail.com>
2019-03-17 19:47:17 -0400

    Totally new Layout

--- assets/JavaScript/java.js
+++ assets/JavaScript/java.js
@@ -1,38 +1,17 @@
-let navbar = document.getElementById("navbar");
-let button = document.getElementById("submitButton");
-let sticky = navbar.offsetTop;
-let isClicked = false;
+AOS.init();
 
+let navbar = document.getElementById("navbar");
 let menuIcon = document.getElementById("menu-icon");
 let dropdownMenu = document.getElementById("navbar");
 let main = document.getElementById("main");
-console.log(main);
+// let sticky = navbar.offsetTop;
 
 menuIcon.addEventListener("click", event => {
-  dropdownMenu.classList.toggle("dropdown-menu");
-  main.classList.toggle("mainDown");
+  dropdownMenu.classList.toggle("dropdown");
 });
-
-// window.onscroll = function() {
-//   fixedNavbar();
-// };
-
-// function fixedNavbar() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky");
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
 
 $(".links").on("click", event => {
   let activeLink = event.target;
-  if ($(activeLink).hasClass("isClicked")) {
-    $(".links").removeClass("active");
-    console.log("links ", $(".links"));
-    console.log("in has class");
-  } else {
-    activeLink.classList.add("active");
-    activeLink.classList.add("isClicked");
-  }
+  $(".links").removeClass("active");
+  activeLink.classList.add("active");
 });
