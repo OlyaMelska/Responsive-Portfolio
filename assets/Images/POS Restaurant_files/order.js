6b90acf2f6101fd118049b4e5ce566ceb862c5b8
candye_sweet <olya.melska@gmail.com>
2019-05-09 12:03:11 -0400

    Adding a new project insted of Liri app

---
+++ assets/Images/POS Restaurant_files/order.js
@@ -0,0 +1,20 @@
+// let addToOrder = () => {
+//   let length = $(".item").length;
+//   console.log("Length ", length);
+
+//   for (let i = 0; i < length; i++) {
+//     console.log("name of item, ", i, " ", $(".item")[i]);
+//     let itemName = $(".item")[i];
+//     console.log(itemName.text());
+//     //   let newOrder = {
+//     //     item: $(".item")[i].val(),
+//     //     itemQty: 1,
+//     //     price: $(".right-price")[i].val()
+//     //   };
+//     //   $.post("/api/orders", newOrder).then(res => {
+//     //     console.log("Succes ", res);
+//     //   });
+//   }
+// };
+
+// $("#send").on("click", addToOrder);
